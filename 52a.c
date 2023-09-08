927f8c6c2a65923bea408a11979f45ce08414e53
Alex <alexnouansacksy@gmail.com>
2023-09-07 19:25:13 -0500

    Update

--- 52a.c
+++ 52a.c
@@ -4,23 +4,24 @@
 
 int main() {
     int length;
-    printf("Length: ");
+    printf("Length:");
     scanf("%d", &length);
 
     int width;
-    printf("Width: ");
+    printf("Width:");
     scanf("%d", &width);
+
 
     int area = length * width;
     int perimeter = (length * 2) + (width * 2);
 
     printf("area is %d\nperimeter is %d", area, perimeter);
 }
-/*
-Length:12
- Width:12
- area is 144
-perimeter is 48
-Process finished with exit code 27
 
- */
+
+// Length:12
+// Width:12
+// area is 144
+// perimeter is 48
+// Process finished with exit code 27
+
