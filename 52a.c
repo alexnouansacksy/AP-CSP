a970c97b0af3a8bc31f78ef994b3c9c9b7c78203
Alex <alexnouansacksy@gmail.com>
2023-09-07 19:21:41 -0500

    Update

--- 52a.c
+++ 52a.c
@@ -1,3 +1,26 @@
-//
+// Prog52a
 // Created by alex on 9/7/2023.
-//
+#include <stdio.h>
+
+int main() {
+    int length;
+    printf("Length: ");
+    scanf("%d", &length);
+
+    int width;
+    printf("Width: ");
+    scanf("%d", &width);
+
+    int area = length * width;
+    int perimeter = (length * 2) + (width * 2);
+
+    printf("area is %d\nperimeter is %d", area, perimeter);
+}
+/*
+Length:12
+ Width:12
+ area is 144
+perimeter is 48
+Process finished with exit code 27
+
+ */
