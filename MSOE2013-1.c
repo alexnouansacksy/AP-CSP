bfe921d3cbd4b1ef0239c0b810782e3b1bfd13fa
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-09-18 09:15:07 -0500

    Update

--- MSOE2013-1.c
+++ MSOE2013-1.c
@@ -3,7 +3,40 @@
 
 #include <stdio.h>
 #include <math.h>
-
+#include <math.h>
 int main() {
+    int res1;
+    int res2;
+    int res3;
+    int voltage;
+    double resistance;
+
+    printf("resistance 1:");
+    scanf("%d", &res1);
+    printf("resistance 2:");
+    scanf("%d", &res2);
+    printf("resistance 3:");
+    scanf("%d", &res3);
+
+    printf("voltage:");
+    scanf("%d", &voltage);
+
+    double current1 = voltage / (double) res1;
+    double current2 = voltage / (double) res2;
+    double current3 = voltage / (double) res3;
+    double currents = current1 + current2 + current3;
+
+    resistance = voltage / currents;
+
+    printf("Resistance is %.2f", resistance);
 
 }
+
+/*
+resistance 1:500
+resistance 2:1000
+resistance 3:2500
+voltage:5
+Resistance is 294.12
+Process finished with exit code 20
+ */
