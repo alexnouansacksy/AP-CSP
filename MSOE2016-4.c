cbf78ba6ed7bb95bb38b8e687f5264c5f3312f62
Alex <alexnouansacksy@gmail.com>
2023-09-18 23:33:10 -0500

    Update from da PC

--- MSOE2016-4.c
+++ MSOE2016-4.c
@@ -1,14 +1,17 @@
 // MSOE 2016 QUESTION 4
 // Created by alex on 9/18/2023.
-
+// THIS TOOK ME 2 HOURS WTF
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
 
-int prime(int a, int b) {
+int relativePrime(int a, int b) {
     if (a == 0) return b;
     int c = b % a;
-    return prime(c, a);
+    return relativePrime(c, a);
+    // THIS WILL KEEP MODDING UNTIL IT FINDS A SHARED FACTOR
+    // WHEN THE RECURSION FINISHES, B WILL BE THAT SHARED FACTOR
+    // IF THE ONLY SHARED FACTOR IS 1, THEN WE KNOW THAT IT IS A RELATIVE PRIME
 }
 
 int main() {
@@ -19,7 +22,9 @@ int main() {
     int primes = 1;
     int i;
     for ( i = 2; i < num; i++) {
-        if(prime(i, num) == 1) primes++;
+        // 1 IS ALWAYS A RELATIVE PRIME SO WE SKIP THAT
+        // WE START WITH 2 AND GO UP TO THE NUMBER WE WANT
+        if(relativePrime(i, num) == 1) primes++;
     }
 
     printf("that number has %d relative primes", primes);
