54a2033da9e2a8facbeff362d4ae7260de955a9f
Alex <alexnouansacksy@gmail.com>
2023-09-19 23:58:12 -0500

    Update

--- StringInterviewQuestion12.py
+++ StringInterviewQuestion12.py
@@ -4,10 +4,19 @@ length = len(words)
 words.reverse()
 print(*words)
 
-# THIS ONE PUTS IT IN A DIFFERENT LINE EACH TIME
-# for x in words:
-#     print(x)
+''' NOTES
+ THIS ONE PUTS IT IN A DIFFERENT LINE EACH TIME
+ for x in words:
+     print(x)
 
-# THIS ONE PUTS IT ON THE SAME LINE BUT LOOKS WEIRD, THE ONE I DID IN THE REAL CODE LOOKS BETTER
-# for x in words:
-#     print(x,end=" ")
+THIS ONE PUTS IT ON THE SAME LINE BUT LOOKS WEIRD, THE ONE I DID IN THE REAL CODE LOOKS BETTER
+ for x in words:
+     print(x,end=" ")
+'''
+
+''' OUTPUT 
+enter the sentence: Java is best programming language
+language programming best is Java
+
+Process finished with exit code 0
+'''
