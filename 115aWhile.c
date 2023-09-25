42c8a8f7da5696a29bff1ce02d5cc67d06c5fe08
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-09-25 09:36:21 -0500

    Update

---
+++ 115aWhile.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int c = 2;
+    while (c <= 36) {
+        printf("%d\n", c);
+        c += 2;
+    }
+}
+/*
+PS C:\Users\Alex Nouansacksy\Desktop\AP-CSP\AP-CSP> .\115aWhile
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+*/
---
+++ 122d.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    for (int i = -12; i < 16; i++) {
+    
+    }
+}
