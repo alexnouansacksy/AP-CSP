4310b8139b0f60e9cd76ed5b62276c49f42dacae
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-10-03 09:26:10 -0500

    Update

---
+++ stringinterview16.cpp
@@ -0,0 +1,42 @@
+// String Interview Question 16
+// Alex Nouansacksy
+// 10/03/2023
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string sentence;
+    string phrase;
+
+
+    printf("Enter the sentence: ");
+    getline(cin, sentence);
+    printf("Enter the phrase: ");
+    getline(cin, phrase);
+
+    int sentenceLength = sentence.size();
+    int phraseLength = phrase.size();
+
+    bool contains = false;
+    for (int i = 0; i < sentenceLength - phraseLength + 1; i++) 
+        if (sentence.substr(i, phraseLength) == phrase) contains = true; 
+
+        
+    if (contains) printf("%s is in the phrase '%s'", phrase.c_str(), sentence.c_str());
+    else printf("'%s' is not in the phrase '%s'", phrase.c_str(), sentence.c_str());
+    
+}
+
+/*
+PS C:\Users\Alex Nouansacksy\Desktop\AP-CSP\AP-CSP> .\stringinterview16
+Enter the sentence: oooooooooalexooooooooo
+Enter the phrase: alex
+alex is in the phrase 'oooooooooalexooooooooo'
+PS C:\Users\Alex Nouansacksy\Desktop\AP-CSP\AP-CSP> .\stringinterview16
+Enter the sentence: ooooooooooooooalexoooooo
+Enter the phrase: hello
+'hello' is not in the phrase 'ooooooooooooooalexoooooo'
+*/
