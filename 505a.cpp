d6165a3c020dc8330b9240da02978bfe74065781
Alex <alexnouansacksy@gmail.com>
2023-10-18 21:48:39 -0500

    update

--- 505a.cpp
+++ 505a.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main() {
     FILE * input;
-    input = fopen("C:\\Users\\Alex Nouansacksy\\Desktop\\AP-CSP\\AP-CSP\\data\\prog505a.txt", "w");
+    input = fopen("C:\\Users\\Alex Nouansacksy\\Desktop\\AP-CSP\\AP-CSP\\data\\prog505a.txt", "r1");
     printf("Enter amount of workers: ");
     int workers;
     scanf("%d", &workers);
 
     string firsts[workers];
     string lasts[workers];
-    string booksread[workers];
+    int booksread[workers];
 
     string first = "";
     string last = "";
-    int books = 0;
+    int books = -1;
     int i = 0;
 
     fscanf(input, "%s %s %d", first, last, &books);
@@ -27,7 +27,7 @@ int main() {
     booksread[i] = books;
 
     printf("%s %s %d", firsts[i], lasts[i], booksread[i]);
-    
+    for (int idk : booksread) printf("\n%d", idk);
 
     
 
