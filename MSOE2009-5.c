de9709f0b7ff28163248e7e8632f5df83d394f8d
Alex Nouansacksy <nouansacks.a@stu.janesville.k12.wi.us>
2023-10-23 09:08:59 -0500

    Update

--- MSOE2009-5.c
+++ MSOE2009-5.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
+void isPrime(int n) {
+    bool prime = true;
+    for (int i = n-1; i > 1; i--) if (n % i == 0) prime = false;
+    if (prime) printf("prime");
+    else printf("not prime");
+}
 
 int main() {
     printf("Enter your number: ");
     int num;
     scanf("%d", &num);
-    bool prime = true;
-    for (int i = num-1; i > 1; i--) if (num % i == 0) prime = false;
-    if (prime) printf("prime");
-    else printf("not prime");
+    isPrime (num);
 }
 
 /*
