bcbc1003ce79f0bd4a8d74a4bd69d9e96e3f9d96
Alex Nouansacksy <alexnouansacksy@gmail.com>
2023-10-30 19:15:42 -0500

    Update

---
+++ Project1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() {
+    int timeremaining = 15 * 60;
+    while (timeremaining != 0) {
+        int minutes = timeremaining / 60;
+        int seconds = timeremaining - minutes * 60;
+        printf("");
+        if(seconds == 0) {
+            printf("\n%d:%d0 remaining", minutes, seconds);
+        } else if (seconds < 10) {
+            printf("\n%d:0%d remaining", minutes, seconds);
+        } else {
+            printf("\n%d:%d remaining", minutes, seconds);
+        }
+        sleep(0.1);
+        timeremaining--;
+    }
+}
