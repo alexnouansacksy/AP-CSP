9dc5ca6123c0059e9aa14dd82d21aca3bbc2d657
Alex Nouansacksy <alexnouansacksy@gmail.com>
2023-12-12 09:08:13 -0600

    Update

--- prog505a.java
+++ prog505a.java
@@ -1,2 +1,18 @@
+// Alex Nouansacksy
+// Prog 505a
+// 12/12/2023
+
+import java.util.*;
+
+import static java.lang.System.out;
+
 public class prog505a {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> books = new ArrayList<String>();
+        ArrayList<String> points = new ArrayList<String>();
+
+        out.println("Hello Wolrd");
+    }
 }
