0febd3fd1e1fc5106c949abe09a4031ee9a443a4
Alex Nouansacksy <alexnouansacksy@gmail.com>
2023-12-12 09:29:12 -0600

    Update

--- prog505a.java
+++ prog505a.java
@@ -7,12 +7,40 @@ import java.util.*;
 import static java.lang.System.out;
 
 public class prog505a {
+
+    public static int calc(int books) {
+        if (books <= 3) return books * 10;
+        else if (books <= 6) return 30 + (books - 3) * 15;
+        else return 30 + 45 + 20 * (books - 6);
+    }
+
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         ArrayList<String> names = new ArrayList<String>();
-        ArrayList<String> books = new ArrayList<String>();
-        ArrayList<String> points = new ArrayList<String>();
+        ArrayList<Integer> books = new ArrayList<Integer>();
+        ArrayList<Integer> points = new ArrayList<Integer>();
 
-        out.println("Hello Wolrd");
-    }
+        out.print("Number of people: ");
+        int cnt = keyboard.nextInt();
+        out.println();
+
+        for (int i = 0; i < cnt; i++) {
+            out.print("First Name: ");
+            String name = keyboard.next();
+            
+            out.print("Last Name: ");
+            name = name + " " + keyboard.next();
+
+            out.print("Books: ");
+            int book = keyboard.nextInt();
+
+            names.add(name);
+            books.add(book);
+            points.add(calc(book));
+
+            out.println();
+        }
+
+        keyboard.close();
+    }    
 }
